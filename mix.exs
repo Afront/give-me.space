2b195485062a948a5c14179516de83511a485307
Afront <3943720+Afront@users.noreply.github.com>
2020-01-20 21:02:41 +0900

    Remove whitespace

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule GiveMeSpace.MixFile do
       app: :give_me_space,
       version: "0.1.0",
       elixir: "~> 1.9",
-      start_permanent: Mix.env == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
